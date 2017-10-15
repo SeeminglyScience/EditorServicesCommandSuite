3344fee776f89043e04141877221ace7f48a99f7
Patrick M. Meinecke <seeminglyscience@gmail.com>
2017-10-15 17:28:42 -0400

    Fix error preference gathering in ThrowError
    Move the preference check to after context is retrieved so we can
    check the preference of the caller.

--- module/Private/ThrowError.ps1
+++ module/Private/ThrowError.ps1
@@ -36,12 +36,6 @@ function ThrowError {
         $Show
     )
     end {
-        # Need to manually check error action because of calling the error methods from a different
-        # cmdlet context. Also reading/setting the error preference variable when the value is "Ignore"
-        # throws, so we get it through variable intrinsics.
-        $errorPreference = $ExecutionContext.SessionState.PSVariable.GetValue('ErrorActionPreference')
-        if ($errorPreference -eq 'Ignore') { return }
-
         if (-not $ErrorContext) {
             foreach ($frame in (Get-PSCallStack)) {
                 if ($frame.Command -eq $MyInvocation.MyCommand.Name) { continue }
@@ -49,6 +43,12 @@ function ThrowError {
             }
             if (-not $ErrorContext) { $ErrorContext = $PSCmdlet }
         }
+
+        $errorPreference = $ErrorContext.SessionState.PSVariable.GetValue('ErrorActionPreference')
+        if ($errorPreference -eq 'Ignore') {
+            return
+        }
+
         if ($PSCmdlet.ParameterSetName -eq 'New') {
             $ErrorRecord = [ErrorRecord]::new($Exception, $Id, $Category, $TargetObject)
         }
