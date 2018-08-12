dc640deb4c8a65c85dce7bbfd2e2351bdaba7b45
Jos Koelewijn <jos.koelewijn@ogd.nl>
2018-08-12 15:04:27 +0200

    Fix attribute names, no need to append 'Attribute' here.

--- src/EditorServicesCommandSuite/CodeGeneration/Refactors/CommandSplatRefactorSettings.cs
+++ src/EditorServicesCommandSuite/CodeGeneration/Refactors/CommandSplatRefactorSettings.cs
@@ -5,24 +5,24 @@ namespace EditorServicesCommandSuite.CodeGeneration.Refactors
     internal class CommandSplatRefactorSettings : RefactorConfiguration
     {
         [Parameter(Position = 0)]
-        [DefaultFromSettingAttribute("CommandSplatRefactor.VariableName", Default = "$null")]
+        [DefaultFromSetting("CommandSplatRefactor.VariableName", Default = "$null")]
         [ValidateNotNullOrEmpty]
         public string VariableName { get; set; }
 
         [Parameter]
-        [DefaultFromSettingAttribute("CommandSplatRefactor.NewLineAfterHashtable", Default = "$true")]
+        [DefaultFromSetting("CommandSplatRefactor.NewLineAfterHashtable", Default = "$true")]
         public SwitchParameter NewLineAfterHashtable { get; set; }
 
         [Parameter]
-        [DefaultFromSettingAttribute("CommandSplatRefactor.AllParameters", Default = "$false")]
+        [DefaultFromSetting("CommandSplatRefactor.AllParameters", Default = "$false")]
         public SwitchParameter AllParameters { get; set; }
 
         [Parameter]
-        [DefaultFromSettingAttribute("CommandSplatRefactor.MandatoryParameters", Default = "$false")]
+        [DefaultFromSetting("CommandSplatRefactor.MandatoryParameters", Default = "$false")]
         public SwitchParameter MandatoryParameters { get; set; }
 
         [Parameter]
-        [DefaultFromSettingAttribute("CommandSplatRefactor.NoHints", Default = "$false")]
+        [DefaultFromSetting("CommandSplatRefactor.NoHints", Default = "$false")]
         public SwitchParameter NoHints { get; set; }
     }
 }
